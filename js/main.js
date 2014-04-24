600dd70098c4e33440e24098199fa7770dab969b
Sylvain Dethier <dethiersylvain@gmail.com>
2014-04-24 21:37:27 +0200

    Added Hover css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+jQuery(document).ready(function($){
+    /* analytics.js track events */
+    $("[data-ga-label]").on('click', function () {
+        var label = $(this).attr('data-ga-label');
+        // do things w/ `data-ga-label`
+        ga('send', 'event', 'link', 'click', label);
+    });
+});
