9194791a575a5cb9421389796ccc9a6d684d92c8
sylvaindethier <dethiersylvain@gmail.com>
2016-08-25 12:09:34 +0200

    refactor(router)

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 // React
-import React from 'react';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
+import router from './router';
 // CSS
 import 'normalize.css';
 import './index.css';
 
 render(
-  <Router history={browserHistory} routes={routes} />,
+  router,
   document.getElementById('root')
 );
--- src/router.jsx
+++ src/router.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Router, browserHistory, Route, IndexRoute } from 'react-router';
+
 // React components
 import App from './components/App';
 import Home from './components/Home';
 import Example from './components/Example';
 import PageNotFound from './components/PageNotFound';
 
-// keep the PageNotFound component at the end of the routes definition
-export default (
+export const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="/example" component={Example} />
 
     <Route path="*" component={PageNotFound} />
   </Route>
+);
+
+export default (
+  <Router history={browserHistory} routes={routes} />
 );
