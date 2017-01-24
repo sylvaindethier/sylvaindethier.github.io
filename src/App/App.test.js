cf63484b60eeb9a7a590d332c31ebba688a19ee9
sylvaindethier <dethiersylvain@gmail.com>
2017-01-24 01:43:08 +0100

    fix: restore default ReactApp

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { render } from 'react-dom'
+import App from './component'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  render(<App />, div)
 })
---
+++ src/App/component.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link, Match, Miss } from 'react-router'
+import { asyncModuleComponent } from '../asyncComponent'
+// media
+import logo from './logo.svg'
+import './App.css'
+
+// define pages as asyncComponent
+const Home = asyncModuleComponent(() => import('../pages/Home'))
+const About = asyncModuleComponent(() => import('../pages/About'))
+const Topics = asyncModuleComponent(() => import('../pages/Topics'))
+const NoMatch = asyncModuleComponent(() => import('../pages/NoMatch'))
+
+const App = () => (
+  <div className='App'>
+    <div className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className='App-intro'>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <ul className='App-nav'>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+      <li><Link to='/topics'>Topics</Link></li>
+      <li><Link to='/no-match'>Not matched</Link></li>
+    </ul>
+
+    <hr />
+    <Match exactly pattern='/' render={(props) => <Home {...props} />} />
+    <Match pattern='/about' render={(props) => <About {...props} />} />
+    <Match pattern='/topics' render={(props) => <Topics {...props} />} />
+    <Miss component={NoMatch} />
+  </div>
+)
+
+export default App
