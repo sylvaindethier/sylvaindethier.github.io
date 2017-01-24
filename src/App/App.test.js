87beebf29b1b4780313c24b18764d42d45572f4f
sylvaindethier <dethiersylvain@gmail.com>
2017-01-24 02:04:14 +0100

    rename

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './component'
+import App from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
