52727f68e3034b774d097f15acdda42836d58f76
sylvaindethier <dethiersylvain@gmail.com>
2017-02-10 18:00:41 +0100

    fix: palette colors

--- src/App/muiTheme.js
+++ src/App/muiTheme.js
@@ -1,12 +1,17 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import {
+  blueGrey500, blueGrey700,
+  lightBlueA200
+} from 'material-ui/styles/colors'
 
 // customise docs:
 // http://www.material-ui.com/#/customization/themes
 // https://github.com/callemall/material-ui/blob/master/src/styles/baseThemes/lightBaseTheme.js
-const theme = {
+const muiTheme = getMuiTheme({
   palette: {
+    primary1Color: blueGrey500,
+    primary2Color: blueGrey700,
+    accent1Color: lightBlueA200
   }
-}
-
-const muiTheme = getMuiTheme(theme)
+})
 export default muiTheme
