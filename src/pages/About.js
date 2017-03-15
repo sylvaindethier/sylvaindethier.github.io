a39d771452ad47a1f6aea972f5e3a7c2c1b40720
sylvaindethier <dethiersylvain@gmail.com>
2017-03-15 19:13:35 +0100

    fix: add About content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, CardHeader, CardTitle, CardText } from 'material-ui/Card'
 
-const text = `
+const htmlMe = `
   <p>Je suis développeur web depuis <b>2008</b>. J'ai commencé sur des projets
   <b><i>backend</i></b> en <code>PHP</code> (<code>Symfony</code>, <code>Zend</code>),
   avec souvent une partie <b><i>frontend</i></b> (<code>JavaScript</code>, <code>jQuery</code>).</p>
@@ -13,6 +13,14 @@ const text = `
   <p>J'ai sourtout collaboré avec de petites équipes où des compétences <b><i>full-stack</i></b>
   sont nécessaires.</p>
 `
+const htmlSite = `
+  <p>Le site est propulsé par <b><code>GitHub Pages</code></b>,
+  développé en <b><i>modern</i> <code>ReactJS</code></b>
+  (<code>NodeJS</code>,
+  <code>babel es6 / es7</code>, <code>Webpack</code>, <code>ESLint, Prettier</code>,
+  <code>ReactJS</code>, <code>React Router</code>, <code>Material UI</code>, ...).</p>
+`
+
 const styles = {
   CardText: { fontSize: 'medium' }
 }
@@ -26,7 +34,12 @@ const About = () => (
     />
     <CardTitle title='À propos' subtitle='De qui je suis.' />
     <CardText style={styles.CardText}>
-      <div dangerouslySetInnerHTML={{__html: text}} />
+      <div dangerouslySetInnerHTML={{__html: htmlMe}} />
+    </CardText>
+
+    <CardTitle title='À propos' subtitle='Du site.' />
+    <CardText>
+      <div dangerouslySetInnerHTML={{__html: htmlSite}} />
     </CardText>
   </Card>
 )
