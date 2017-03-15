91178f83d71671c107de5b8f058b8c86da2b9ac7
sylvaindethier <dethiersylvain@gmail.com>
2017-03-15 18:04:20 +0100

    fix: Home AbourCard

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,71 @@
 import React from 'react'
-import Paper from 'material-ui/Paper'
-import { Card, CardHeader, CardTitle, CardText } from 'material-ui/Card'
+import { Card, CardHeader, CardTitle, CardText, CardActions } from 'material-ui/Card'
+import RaisedButton from 'material-ui/RaisedButton'
+import FontIcon from 'material-ui/FontIcon'
+import Link from 'react-router-dom/Link'
+import urls from '../urls'
 
-const HomePaper = () => (
-  <Paper zDepth={2}>
-    <h2>Développeur d'application web.</h2>
-    <p>JavaScript, NodeJS, ReactJS. UI/UX.</p>
-  </Paper>
+const styles = {
+  Card: { margin: '24px 0' }
+}
+
+const AboutCard = () => (
+  <Card style={styles.Card}>
+    <CardHeader
+      title='Sylvain Dethier'
+      subtitle='WebApp Developer'
+      avatar='icons/favicon.png'
+    />
+    <CardTitle title='À propos' subtitle='De qui je suis.' />
+    <CardActions>
+      <RaisedButton
+        label={<Link to={urls.about}>Voir la page</Link>}
+        icon={<FontIcon className='material-icons'>person</FontIcon>}
+        secondary
+      />
+    </CardActions>
+  </Card>
+)
+
+const ResumeCard = () => (
+  <Card>
+    <CardHeader
+      title='URL Avatar'
+      subtitle='Subtitle'
+      avatar='images/jsa-128.jpg'
+    />
+    <CardTitle title='Card title' subtitle='Card subtitle' />
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+  </Card>
+)
+
+const ContactCard = () => (
+  <Card>
+    <CardHeader
+      title='URL Avatar'
+      subtitle='Subtitle'
+      avatar='images/jsa-128.jpg'
+    />
+    <CardTitle title='Card title' subtitle='Card subtitle' />
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+  </Card>
 )
 
 const Home = () => (
   <div className='page-content'>
-    <HomePaper />
-    <Card>
-      <CardHeader
-        title='URL Avatar'
-        subtitle='Subtitle'
-        avatar='images/jsa-128.jpg'
-      />
-      <CardTitle title='Card title' subtitle='Card subtitle' />
-      <CardText>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
-    </Card>
+    <AboutCard />
+    <ResumeCard />
+    <ContactCard />
   </div>
 )
 
