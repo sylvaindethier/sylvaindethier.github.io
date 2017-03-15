686058f798b570cb50f16aa48d631aaa3bcbba63
sylvaindethier <dethiersylvain@gmail.com>
2017-03-15 17:29:56 +0100

    fix: About pages text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Card, CardHeader, CardText } from 'material-ui/Card'
+
+const text = `
+  <p>Je suis développeur web depuis <b>2008</b>. J'ai commencé sur des projets
+  <b><i>backend</i></b> en <code>PHP</code> (<code>Symfony</code>, <code>Zend</code>),
+  avec souvent une partie <b><i>frontend</i></b> (<code>JavaScript</code>, <code>jQuery</code>).</p>
+
+  <p>Depuis <b>2015</b>, je m'oriente en tant que développeur WebApp
+  <b><i>frontend / backend</i></b> en <code>JavaScript</code>
+  (<code>NodeJS</code>, <code>React</code>, UI, UX).</p>
+
+  <p>J'ai sourtout collaboré avec de petites équipes où des compétences <b><i>full-stack</i></b>
+  sont nécessaires.</p>
+`
+const styles = {
+  CardText: { fontSize: 'medium' }
+}
 
 const About = () => (
-  <div className='page-content'>
-    <h2>À propos.</h2>
-  </div>
+  <Card zDepth='2'>
+    <CardHeader
+      title='À propos'
+      subtitle='de moi'
+      avatar='icons/favicon.png'
+    />
+    <CardText style={styles.CardText}>
+      <div dangerouslySetInnerHTML={{__html: text}} />
+    </CardText>
+  </Card>
 )
 
 export default About
