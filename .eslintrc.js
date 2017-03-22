55a93b0d0ed3dec8cd1a437531b123d2200b3754
sylvaindethier <dethiersylvain@gmail.com>
2017-03-22 18:33:48 +0100

    fix: eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
 
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
 
   env: {
     browser: true,
@@ -13,7 +13,7 @@ module.exports = {
 
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
       generators: true,
@@ -21,74 +21,70 @@ module.exports = {
     }
   },
 
-  plugins: ["import", "flowtype", "jsx-a11y", "react", "prettier"],
+  plugins: ['import', 'flowtype', 'jsx-a11y', 'react'],
 
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:flowtype/recommended",
-    "prettier",
-    "prettier/flowtype",
-    "prettier/react",
-    "standard",
-    "standard-react"
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:flowtype/recommended',
+    'standard',
+    'standard-react'
   ],
 
   settings: {
-    "import/ignore": ["node_modules"],
-    "import/extensions": [".js"],
-    "import/resolver": {
+    'import/ignore': ['node_modules'],
+    'import/extensions': ['.js'],
+    'import/resolver': {
       node: {
-        extensions: [".js", ".json"]
+        extensions: ['.js', '.json']
       }
     }
   },
 
   rules: {
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/
-    "import/default": "error",
-    "import/export": "error",
-    "import/named": "error",
-    "import/namespace": "error",
-    "import/no-amd": "error",
-    "import/no-duplicates": "error",
-    "import/no-extraneous-dependencies": "error",
-    "import/no-named-as-default": "error",
-    "import/no-named-as-default-member": "error",
-    "import/no-unresolved": ["error", { commonjs: true }],
-    "import/no-webpack-loader-syntax": "error",
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/no-amd': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-named-as-default': 'error',
+    'import/no-named-as-default-member': 'error',
+    'import/no-unresolved': ['error', {commonjs: true}],
+    'import/no-webpack-loader-syntax': 'error',
 
     // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
-    "react/jsx-equals-spacing": ["warn", "never"],
-    "react/jsx-no-duplicate-props": ["error", { ignoreCase: true }],
-    "react/jsx-no-undef": "error",
-    "react/jsx-pascal-case": [
-      "warn",
+    'react/jsx-equals-spacing': ['warn', 'never'],
+    'react/jsx-no-duplicate-props': ['error', {ignoreCase: true}],
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': [
+      'warn',
       {
         allowAllCaps: true,
         ignore: []
       }
     ],
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "react/no-danger-with-children": "error",
-    "react/no-deprecated": "error",
-    "react/no-direct-mutation-state": "error",
-    "react/no-is-mounted": "error",
-    "react/react-in-jsx-scope": "error",
-    "react/require-render-return": "error",
-    "react/style-prop-object": "error",
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-danger-with-children': 'error',
+    'react/no-deprecated': 'error',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-is-mounted': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/require-render-return': 'error',
+    'react/style-prop-object': 'error',
 
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
-    "jsx-a11y/aria-role": "warn",
-    "jsx-a11y/img-has-alt": "warn",
-    "jsx-a11y/img-redundant-alt": "warn",
-    "jsx-a11y/no-access-key": "warn",
+    'jsx-a11y/aria-role': 'warn',
+    'jsx-a11y/img-has-alt': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/no-access-key': 'warn',
 
     // https://github.com/gajus/eslint-plugin-flowtype
-    "flowtype/define-flow-type": "warn",
-    "flowtype/require-valid-file-annotation": "warn",
-    "flowtype/use-flow-type": "warn"
+    'flowtype/define-flow-type': 'warn',
+    'flowtype/require-valid-file-annotation': 'warn',
+    'flowtype/use-flow-type': 'warn'
   }
-};
+}
