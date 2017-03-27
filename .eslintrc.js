5f0687d499e8ebe223e836d21afcb05c04f1d2a7
sylvaindethier <dethiersylvain@gmail.com>
2017-03-27 16:05:59 +0200

    fix: eslint + prettier using eslint-plugin-prettier to make the process editor agnostic

--- .eslintrc.js
+++ .eslintrc.js
@@ -88,6 +88,11 @@ module.exports = {
     // https://github.com/gajus/eslint-plugin-flowtype
     'flowtype/define-flow-type': 'warn',
     'flowtype/require-valid-file-annotation': 'warn',
-    'flowtype/use-flow-type': 'warn'
+    'flowtype/use-flow-type': 'warn',
+
+    // https://github.com/not-an-aardvark/eslint-plugin-prettier
+    'prettier/prettier': ['error', {
+
+    }]
   }
 }
