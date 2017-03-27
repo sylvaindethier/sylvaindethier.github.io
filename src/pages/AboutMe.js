f9047821c3abb7fc30f2bfb913aba68f297097cf
sylvaindethier <dethiersylvain@gmail.com>
2017-03-27 17:28:16 +0200

    fix: About texts

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const AboutMe = () => (
+  <section>
+    <p>
+      Développeur web depuis
+      {" "}
+      <b>2008</b>
+      , j'ai commencé sur des projets
+      {" "}
+      <i><b>backend</b></i>
+      .Le plus souvent avec une intégration
+      {" "}
+      <i><b>frontend</b></i>
+      .
+      <br />
+      J'ai travaillé pendant plus de
+      {" "}
+      <b>6 ans</b>
+      {" "}
+      avec
+      {" "}
+      <b>PHP</b>
+      ,
+      {" "}
+      <b>Symfony</b>
+      , Zend, HTML, CSS, JavaScript, jQuery.
+    </p>
+
+    <p>
+      Depuis
+      {" "}
+      <b>2015</b>
+      , je m'oriente en tant que développeur WebApp
+      {" "}
+      <i><b>frontend / backend</b></i>
+      {" "}
+      en
+      {" "}
+      <b>JavaScript / NodeJS</b>
+      ,
+      {" "}
+      <b>React</b>
+      .
+    </p>
+
+    <p>
+      J'ai sourtout collaboré avec de petites équipes, des compétences
+      {" "}
+      <i><b>full-stack</b></i>
+      {" "}
+      voire
+      {" "}
+      <i>DevOps</i>
+      {" "}
+      sont nécessaires.
+    </p>
+  </section>
+);
+
+export default AboutMe;
---
+++ src/pages/AboutSite.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const AboutSite = () => (
+  <section>
+    <p>
+      Le site est propulsé par
+      {" "}
+      <b>GitHub Pages</b>
+      , développé en
+      {" "}
+      <b>ReactJS</b>
+      ,
+      {" "}
+      <b>React Router v4</b>
+      ,
+      {" "}
+      <b>Webpack v2</b>
+      ,
+      {" "}
+      <b>ESLint / Prettier</b>
+      ,
+      {" "}
+      <b>Material-UI</b>
+      , ….
+    </p>
+  </section>
+);
+
+export default AboutSite;
