58eb9a227a585f2a2376e4312697b4340ee22786
sylvaindethier <dethiersylvain@gmail.com>
2017-03-29 23:13:27 +0200

    fix: missing react-icons

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -1,20 +1,47 @@
 import React from "react";
 import { grey900, lightWhite } from "material-ui/styles/colors";
+import IconButton from "material-ui/IconButton";
+import CopyrightIcon from "react-icons/md/copyright";
+import GitHubIcon from "react-icons/fa/github";
+import TwitterIcon from "react-icons/fa/twitter";
 import urls from "../urls";
 
-const footerStyle = {
-  backgroundColor: grey900,
-  color: lightWhite
+const styles = {
+  footer: {
+    backgroundColor: grey900,
+    color: lightWhite
+  },
+  mediumIcon: {
+    width: 48,
+    height: 48,
+    color: lightWhite
+  },
+  medium: {
+    width: 72,
+    height: 96,
+    padding: "24px 12px"
+  }
 };
 
 const Footer = () => (
-  <footer className="mini-footer" style={footerStyle}>
+  <footer className="mini-footer" style={styles.footer}>
     <div className="mini-footer--left" />
     <div className="mini-footer--center">
-      © Sylvain Dethier.
-      Propulsé par <a href="https://pages.github.com/">GitHub Pages</a>.
-      <a href={urls.twitter}>Twitter</a>
-      <a href={urls.github}>GitHub</a>
+      <CopyrightIcon /> Sylvain Dethier.
+      <IconButton
+        iconStyle={styles.mediumIcon}
+        style={styles.medium}
+        href={urls.twitter}
+      >
+        <TwitterIcon />
+      </IconButton>
+      <IconButton
+        iconStyle={styles.mediumIcon}
+        style={styles.medium}
+        href={urls.github}
+      >
+        <GitHubIcon />
+      </IconButton>
     </div>
     <div className="mini-footer--right" />
   </footer>
