44f6234e8b8f9d8a9d8c0782cbfca8e41af347d6
sylvaindethier <dethiersylvain@gmail.com>
2017-03-29 16:05:37 +0200

    fiw: missing urls

--- src/urls.js
+++ src/urls.js
@@ -7,7 +7,9 @@ const urls = {
   // externals
   linkedin: "https://linked.com/in/sylvaindethier",
   github: "https://github.com/sylvaindethier",
-  twitter: "https://twitter.com/sylvaindethier"
+  gitshowcase: "https://gitshowcase.com/sylvaindethier",
+  twitter: "https://twitter.com/sylvaindethier",
+  email: "mailto:dethiersylvain@gmail.com"
 };
 
 export default urls;
