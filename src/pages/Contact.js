0668f50a9de002790448e36084e1428b674dde8b
sylvaindethier <dethiersylvain@gmail.com>
2017-03-29 16:06:29 +0200

    fix: add Contact content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,44 @@
 import React from "react";
+import { Card, CardHeader, CardText } from "material-ui/Card";
+import RaisedButton from "material-ui/RaisedButton";
+import FontIcon from "material-ui/FontIcon";
+import urls from "../urls";
 
-const Contact = () => <h2>Contact.</h2>;
+const styles = {
+  RaisedButton: {
+    marginLeft: "1em",
+    marginRight: "1em"
+  }
+};
+
+const Contact = () => (
+  <Card zDepth={2}>
+    <CardHeader
+      title="Contact"
+      subtitle="Me contacter."
+      avatar={<FontIcon className="material-icons">contact_mail</FontIcon>}
+    />
+    <CardText>
+      <RaisedButton
+        href={urls.email}
+        label="Email"
+        icon={<FontIcon className="material-icons">contact_mail</FontIcon>}
+        style={styles.RaisedButton}
+      />
+      <RaisedButton
+        href={urls.twitter}
+        label="Twitter"
+        icon={<FontIcon className="fa fa-twitter" />}
+        style={styles.RaisedButton}
+      />
+      <RaisedButton
+        href={urls.linkedin}
+        label="LinkedIn"
+        icon={<FontIcon className="fa fa-linkedin" />}
+        style={styles.RaisedButton}
+      />
+    </CardText>
+  </Card>
+);
 
 export default Contact;
