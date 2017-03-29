6bbd0d7cf030ffb4715b8a144baaed405f17d7a6
sylvaindethier <dethiersylvain@gmail.com>
2017-03-29 16:05:56 +0200

    fiw: add some text

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { grey900, lightWhite } from "material-ui/styles/colors";
+import urls from "../urls";
 
 const footerStyle = {
   backgroundColor: grey900,
@@ -9,6 +10,12 @@ const footerStyle = {
 const Footer = () => (
   <footer className="mini-footer" style={footerStyle}>
     <div className="mini-footer--left" />
+    <div className="mini-footer--center">
+      © Sylvain Dethier.
+      Propulsé par <a href="https://pages.github.com/">GitHub Pages</a>.
+      <a href={urls.twitter}>Twitter</a>
+      <a href={urls.github}>GitHub</a>
+    </div>
     <div className="mini-footer--right" />
   </footer>
 );
