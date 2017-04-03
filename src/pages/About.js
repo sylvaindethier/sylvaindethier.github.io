aff17f214ef836e1941ea781d41d4a569af12037
sylvaindethier <dethiersylvain@gmail.com>
2017-04-03 17:03:46 +0200

    fix: move AboutMe intto About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,53 @@
 /* @flow */
 import React from "react";
 import { Card, CardHeader, CardTitle, CardText } from "material-ui/Card";
-import AboutMe from "./AboutMe";
+
+const AboutMe = (
+  <section>
+    <p>
+      Développeur web depuis 2008, j'ai commencé sur des projets
+      {" "}
+      <i><b>backend</b></i>
+      . Le plus souvent avec une intégration
+      {" "}
+      <i><b>frontend</b></i>
+      .
+      <br />
+      J'ai travaillé pendant plus de 6 ans avec
+      {" "}
+      <b>PHP</b>
+      ,
+      {" "}
+      <b>Symfony</b>
+      , Zend, HTML, CSS, JavaScript, jQuery.
+    </p>
+
+    <p>
+      Depuis <b>2015</b>
+      , je m'oriente en tant que développeur WebApp
+      {" "}
+      <i><b>frontend / backend</b></i>
+      {" "}
+      en
+      {" "}
+      <b>JavaScript / NodeJS</b>
+      ,
+      {" "}
+      <b>React</b>, Angular, Express, ….
+    </p>
+
+    <p>
+      J'ai sourtout collaboré avec de petites équipes, des compétences
+      {" "}
+      <i><b>full-stack</b></i>
+      ,
+      {" "}
+      <i>DevOps</i>
+      {" "}
+      sont nécessaires.
+    </p>
+  </section>
+);
 
 const styles = {
   CardText: { fontSize: "medium" }
@@ -12,12 +58,12 @@ const About = () => (
     <CardHeader
       title="Sylvain Dethier"
       subtitle="WebApp Developer"
-      avatar="icons/favicon.png"
+      avatar="/icons/favicon.png"
     />
 
     <CardTitle title="À propos" subtitle="Qui je suis." />
     <CardText style={styles.CardText}>
-      <AboutMe />
+      {AboutMe}
     </CardText>
   </Card>
 );
